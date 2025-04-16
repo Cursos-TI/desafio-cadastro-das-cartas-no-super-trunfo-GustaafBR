2eedfb9b6332f356f89d3c120700be6a886e3149
GustaafBR <gustavo2006af@gmail.com>
2025-04-16 19:56:18 +0000

    Desafio aventureiro feito

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+int main(){
     char estado1[50], estado2[50]; //string
     char codigocarta1[50], codigocarta2[50]; //string
     char nomedacidade1[150], nomedacidade2[150]; //string
@@ -8,8 +8,9 @@ int main() {
     int numeroponturisticos1, numeroponturisticos2; //numero inteiro
     float areakm21, areakm22; //numero decimal (com vírgula)
     float PIB1, PIB2; //numero decimal (com vírgula)
+    float PIBPC1, PIBPC2; //numero decimal (com vírgula)
+    float DensDem1, DensDem2; //numero decimal (com vírgula)
 
-    printf("Desafio nivel novato - Super Trunfo \n");
     printf("Registrando Dados dos estados \n");
     printf("------------------------------------------------------------------------------------------------------------------------ \n");
 //-----------------------------------------------------------------------------------------------------------------------    
@@ -61,17 +62,29 @@ int main() {
 //------------------------------------------------------------------------------------------------------------------------
     printf("------------------------------------------------------------------------------------------------------------------------ \n");
 //------------------------------------------------------------------------------------------------------------------------    
+    DensDem1 = populacao1 / areakm21; //Calcula a Densidade Demográfica e entrega o resultado em habitantes por quilômetro quadrado da Carta 1
+    PIBPC1 = PIB1 / populacao1; //Calcula o PIB per Capita da Carta 1 
+//------------------------------------------------------------------------------------------------------------------------
+    DensDem2 = populacao2 / areakm22; //Calcula a Densidade Demográfica e entrega o resultado em habitantes por quilômetro quadrado da Carta 2
+    PIBPC2 = PIB2 / populacao2; //Calcula o PIB per Capita da Carta 2 
+//------------------------------------------------------------------------------------------------------------------------
     printf("Carta 1 \n");
     printf("\n");
     printf("Estado: %s \n", estado1);
     printf("Código: %s \n", codigocarta1);
     printf("Nome da Cidade: %s \n", nomedacidade1);
     printf("População: %d \n", populacao1);
-    printf("Área: %f km² \n", areakm21);
-    printf("PIB: %f bilhões de reais \n", PIB1);
+    printf("Área: %.2f km² \n", areakm21);
+    printf("PIB: %.2f bilhões de reais \n", PIB1);
     printf("Números de Pontos Turísticos: %d \n", numeroponturisticos1);
+    printf("Densidade Demográfica: %.2f hab/km² \n", DensDem1);
+    printf("PIB per Capita: %.2f reais \n", PIBPC1);
 //------------------------------------------------------------------------------------------------------------------------
-    printf("------------------------------------------------------------------------------------------------------------------------ \n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
 //------------------------------------------------------------------------------------------------------------------------
     printf("Carta 2 \n");
     printf("\n");
@@ -79,7 +92,9 @@ int main() {
     printf("Código: %s \n", codigocarta2);
     printf("Nome da Cidade: %s \n", nomedacidade2);
     printf("População: %d \n", populacao2);
-    printf("Área: %f km² \n", areakm22);
-    printf("PIB: %f bilhões de reais \n", PIB2);
+    printf("Área: %.2f km² \n", areakm22);
+    printf("PIB: %.2f bilhões de reais \n", PIB2);
     printf("Números de Pontos Turísticos: %d \n", numeroponturisticos2);
+    printf("Densidade Demográfica: %.2f hab/km² \n", DensDem2);
+    printf("PIB per Capita: %.2f reais \n", PIBPC2);    
 }
